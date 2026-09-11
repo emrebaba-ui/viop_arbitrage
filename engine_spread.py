4b7b0f1f4524207fd618bb6a0bd0c5f9c2d185b9
emrebaba-ui <emrebaba95@gmail.com>
2026-09-11 22:36:21 +0300

    v1.110: 3 days factor for stocks, potential profit are added and readability is increased

--- engine_spread.py
+++ engine_spread.py
@@ -2,7 +2,7 @@ import pandas as pd
 from itertools import combinations
 
 class FutureSpreadEngine:
-    def __init__(self, target_rate=0.35, min_volume_tl=50000, commission_rate=0.001):
+    def __init__(self, target_rate=0.37, min_volume_tl=50000, commission_rate=0.001):
         self.target_rate = target_rate 
         self.min_volume_tl = min_volume_tl
         self.commission_rate = commission_rate
@@ -43,62 +43,67 @@ class FutureSpreadEngine:
                 is_usd_asset = asset.endswith('USD') # type: ignore
                 fx = c1.get('USD_Rate', 1.0) if is_usd_asset else 1.0
 
-                convergence_ratio = c1['Days'] / c2['Days'] 
+                if multiplier == 100: # Stocks must be sold ~3 days earlier
+                    convergence_ratio = (c1['Days'] - 3) / (c2['Days'])
+                else:
+                    convergence_ratio = c1['Days'] / c2['Days'] 
                 
-                # --- BUY SPREAD ---
+                # -------------- BUY SPREAD --------------
                 implied_rate_buy = (c2['ask'] / c1['bid']) ** (365 / days_diff) - 1
                 
-                fair_far_buy_spread = c1['bid'] * ((1 + asset_rate) ** (days_diff / 365))
-                total_mispricing_buy = fair_far_buy_spread - c2['ask']
+                theoretical_price_buy = c1['bid'] * ((1 + asset_rate) ** (days_diff / 365))
+                spread_buy = theoretical_price_buy - c2['ask']
                 
-                # total divergence * time at hand
-                gross_profit_buy = total_mispricing_buy * convergence_ratio * multiplier * fx
+                gross_profit_buy = spread_buy * multiplier * fx
+                gross_profit_buy_conv = gross_profit_buy * convergence_ratio
                 comm_buy = (c1['bid'] + c2['ask']) * multiplier * self.commission_rate * fx
-                net_profit_buy = gross_profit_buy - comm_buy
+                pot_profit_buy = gross_profit_buy - comm_buy
+                net_profit_buy = gross_profit_buy_conv - comm_buy
                 
-                # --- SELL SPREAD ---
+                # -------------- SELL SPREAD --------------
                 implied_rate_sell = (c2['bid'] / c1['ask']) ** (365 / days_diff) - 1
                 
-                fair_far_sell_spread = c1['ask'] * ((1 + asset_rate) ** (days_diff / 365))
-                total_mispricing_sell = c2['bid'] - fair_far_sell_spread
+                theoretical_price_sell = c1['ask'] * ((1 + asset_rate) ** (days_diff / 365))
+                spread_sell = c2['bid'] - theoretical_price_sell
                 
-                # total divergence * time at hand
-                gross_profit_sell = total_mispricing_sell * convergence_ratio * multiplier * fx
+                gross_profit_sell = spread_sell * multiplier * fx
+                gross_profit_sell_conv = gross_profit_sell * convergence_ratio
                 comm_sell = (c1['ask'] + c2['bid']) * multiplier * self.commission_rate * fx
-                net_profit_sell = gross_profit_sell - comm_sell
+                pot_profit_sell = gross_profit_sell - comm_sell
+                net_profit_sell = gross_profit_sell_conv - comm_sell
                 
                 if net_profit_buy > 0 and net_profit_buy > net_profit_sell:
                     near_action, near_price = f'SELL: {c1['Contract']}', c1['bid']
                     far_action, far_price = f'BUY: {c2['Contract']}', c2['ask']
                     net_profit = net_profit_buy
+                    pot_profit = pot_profit_buy
                     total_comm = comm_buy
-                    type_str = "BUY SPREAD"
                     final_implied = implied_rate_buy
                 elif net_profit_sell > 0 and net_profit_sell > net_profit_buy:
                     near_action, near_price = f'BUY: {c1['Contract']}', c1['ask']
                     far_action, far_price = f'SELL: {c2['Contract']}', c2['bid']
                     net_profit = net_profit_sell
+                    pot_profit = pot_profit_sell
                     total_comm = comm_sell
-                    type_str = "SELL SPREAD"
                     final_implied = implied_rate_sell
                 else:
                     continue
                 
                 req_capital = max(c1['Req_Capital'], c2['Req_Capital'])
-                holding_days = max(c1['Days'], 1) 
+                holding_days = c1['Days']
                 daily_profit = net_profit / holding_days
                 daily_roi = daily_profit * 100 / req_capital
                 
                 spread_opportunities.append({
                     'Asset': asset,
-                    'Type': type_str,
                     'Near_Action': f"{near_action} @ {near_price:.2f}",
                     'Far_Action': f"{far_action} @ {far_price:.2f}",
-                    'Hold_Days': holding_days,
+                    'Hold': holding_days,
                     'Implied_%': final_implied * 100,
                     'Req_Capital': req_capital,
                     'Total_Comm': total_comm,
                     'Net_Profit': net_profit,
+                    'Pot_Profit': pot_profit,
                     'Daily_Profit': daily_profit,
                     'Daily_ROI_%': daily_roi
                 })
